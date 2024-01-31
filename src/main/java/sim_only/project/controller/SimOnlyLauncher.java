9c57b4d8550d6413350a7a8df2c44c4e909f2d1f
rutgerhanze <152165901+rutgerhanze@users.noreply.github.com>
2024-01-31 14:23:53 +0100

    create method stubs

--- src/main/java/sim_only/project/controller/SimOnlyLauncher.java
+++ src/main/java/sim_only/project/controller/SimOnlyLauncher.java
@@ -6,34 +6,5 @@ import java.util.Scanner;
 
 public class SimOnlyLauncher {
 
-    final static double SUITABLE_PERCENTAGE = 0.2;
-
-    public static void main(String[] args) {
-        System.out.println("Uitwerking van Gerke de Boer");
-        // Variabelen declareren en initialiseren waar nodig
-        int monthAmount = 4;
-        Scanner input = new Scanner(System.in);
-        Consumption[] consumptions = new Consumption[monthAmount];
-        consumptions[0] = new Consumption("Juli");
-        consumptions[1] = new Consumption("Augustus");
-        consumptions[2] = new Consumption("September");
-        consumptions[3] = new Consumption("Oktober");
-
-        for (int i = 0; i < monthAmount; i++) {
-            System.out.println("Geef je dataverbruik in MB per maand:");
-            System.out.print(consumptions[i].getMonth() + ": ");
-            consumptions[i].setMbQuantity(input.nextInt());
-        }
-
-        System.out.println();
-        // Bereken voor alle maanden op basis van het verbruik de kosten
-        for (int i = 0; i < monthAmount; i++) {
-            consumptions[i].calculateCosts();
-        }
-
-        System.out.printf("%-14s%5s%7s \n", "MAAND", "MB", "KOSTEN");
-        for (int i = 0; i < monthAmount; i++) {
-            consumptions[i].showUsage();
-        }
-    }
+    public static void main(String[] args) {    }
 }
